a443b794f1c22e0461ce0ebfb5ae850976acbf7d
Rasheev <rashif.aunur@ui.ac.id>
2023-10-29 21:25:05 +0700

    add 'kembali buku' feature

--- pinjambuku/urls.py
+++ pinjambuku/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from pinjambuku.views import show_pinjam_buku, peminjaman
+from pinjambuku.views import show_pinjam_buku, peminjaman, pengembalian
 
 app_name = 'pinjambuku'
 
 urlpatterns = [
     path('', show_pinjam_buku, name='show_pinjam_buku'),
     path('peminjaman/', peminjaman, name='peminjaman'),
+    path('pengembalian/', pengembalian, name='pengembalian')
 ]
