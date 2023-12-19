b7ebbe0e1c321fa9f43448d0a2f5c4c2a57a9e44
cnotgate <49557224+cnotgate@users.noreply.github.com>
2023-12-20 02:04:25 +0700

    donate

--- donasibuku/urls.py
+++ donasibuku/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from donasibuku.views import show_donasi_buku
+from donasibuku.views import show_donasi_buku, donasi_buku_mobile
 
 app_name = 'donasibuku'
 
 urlpatterns = [
     path('', show_donasi_buku, name='show_donasi_buku'),
+    path('donasi-mobile/', donasi_buku_mobile, name='donasi_buku_mobile'),
 ]
--- donasibuku/views.py
+++ donasibuku/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_http_methods
 from django.views.decorators.csrf import csrf_exempt
 from django.urls import reverse
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, JsonResponse
 
 # Create your views here.
 
@@ -26,3 +26,23 @@ def show_donasi_buku(request):
         'form': form
     }
     return render(request, "donasibuku.html", context)
+
+
+@csrf_exempt
+@login_required(login_url='/authentication/login/')
+def donasi_buku_mobile(request):
+    form = BookForm(request.POST or None)
+    if form.is_valid() and request.method == "POST":
+        book = form.save(commit=False)
+        book.for_sale = False
+        book.save()
+        user = request.user
+        user.points += 50
+        user.save()
+        return JsonResponse({
+            "success": True,
+        }, status=201)
+
+    return JsonResponse({
+        "success": False,
+    }, status=301)
